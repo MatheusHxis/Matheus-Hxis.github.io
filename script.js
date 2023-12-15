030a5e4ecb76740f489812e2c33b188de99fdf45
Matheus Henrique <152836672+MatheusHxis@users.noreply.github.com>
2023-12-15 14:28:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const chk = document.getElementById('chk')
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
+
+document.getElementById('click-element').addEventListener('click', playClickSound);
+
+function playClickSound() {
+    var audio = document.getElementById('click-sound');
+    audio.play();
+}
